d2aa5cbd6810941011fa8232efe2a915a293f8d0
Tess Huang <tesshuangxj@yahoo.com>
2020-04-17 11:55:06 -0700

    Added Loading and data displaying with marker

--- src/components/MapDashboard.js
+++ src/components/MapDashboard.js
@@ -1,39 +1,66 @@
 import React from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+import { Map, TileLayer, Marker, Popup  } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { fetchData } from '../api/fetch'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default function MapDashboard () {
-  const [data, setData] = React.useState(null)
+  const [coronaData, setCoronaData] = React.useState(null)
   const [error, setError] = React.useState(null)
+  const [loading, setLoading] = React.useState(true)
 
   const location = {
     lat: 51.505,
     lng: -0.09,
-    zoom: 6,
+    zoom: 3,
   }
-  
+
   React.useEffect(() => {
     fetchData()
-      .then(data => setData(data))
+      .then(data => {
+        setCoronaData(data)
+        setLoading(false)
+        setError(null)
+      })
       .catch(e => {
         console.warn(e)
         setError('Sorry, can not fetch the latest data.')
+        setLoading(false)
       })
   }, [])
 
-  if(error) {
-    return <div>{error}</div>
+  if(loading) {
+    return <div style={{height: '100vh'}}>Fetching data...</div>
   }
 
+  if(error) {
+    return <div style={{height: '100vh'}}>{error}</div>
+  }
+
+  console.log(coronaData);
   return (
-    <div >
-      <Map center={[location.lat, location.lng]} zoom={location.zoom} style={{height: '500px'}}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </Map>
-    </div>
+    <Map center={[location.lat, location.lng]} zoom={location.zoom} style={{height: '100vh'}}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {coronaData.map(data => (
+        <Marker position={[data.countryInfo.lat, data.countryInfo.long]} key={data.countryInfo._id}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>))
+      }
+    </Map>
+
   )
 }
