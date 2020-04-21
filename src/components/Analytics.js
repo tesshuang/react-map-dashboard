3b068a40e7d9c4553ba20f57ea9eedbcf5651275
Tess Huang <tesshuangxj@yahoo.com>
2020-04-20 17:11:44 -0700

    Added context to click on map

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -7,6 +7,11 @@ export default function Analytics ({ coronaData }) {
   // console.log(top10Countries);
   return (
     <div className='analytics'>
+      <h6>
+        <img src={require('../images/ic_info.svg')} alt='more info' 
+          style={{ verticalAlign: 'sub', width: '16px', marginRight: '5px' }}/>
+        Click on the map to learn more details
+      </h6>
       <h3>Top 10 Countries of today</h3>
       {top10Countries.map((data, key) => (
         <div className='top10' key={data.country}>
