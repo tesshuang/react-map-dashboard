4c04f043f447d782087d77dfbeaef01e6af4e715
Tess Huang <tesshuangxj@yahoo.com>
2020-12-06 21:43:28 -0800

    Fix typo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header () {
   return (
     <header>
-      <h2>Convid-19 Status Update</h2>
+      <h2>Covid-19 Status Update</h2>
     </header>
   )
 }
